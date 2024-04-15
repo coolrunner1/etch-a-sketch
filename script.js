6fdfcbd14d9acbc04a8dd34ed87782d24b610974
waurttttt@gmail.com <igor@ArchIgor.netis>
2024-04-15 09:48:25 +0300

    finished theme change

--- script.js
+++ script.js
@@ -4,15 +4,23 @@ const themeChange = () => {
 }
 
 const themebtn=document.querySelector("#theme");
+const divImg = document.createElement("div");
+divImg.style.width="100%";
+divImg.style.height="100%";
+divImg.style.backgroundImage = "url('https://www.svgrepo.com/show/20546/sun.svg')";
+divImg.style.backgroundRepeat= "no-repeat";
+divImg.style.backgroundSize = "cover";
+themebtn.appendChild(divImg);
 let themeMode=0;
 themebtn.onclick = () => {
-    if (!themeMode){
-        themeMode=1;
+    if (themeMode){
+        themeMode=0;
         document.body.style.backgroundColor = "#f9faf8";
+        divImg.style.backgroundImage = "url('https://www.svgrepo.com/show/20546/sun.svg')";
     }
     else {
-        themeMode=0;
+        themeMode=1;
         document.body.style.backgroundColor = "#181818";
+        divImg.style.backgroundImage = "url('https://www.svgrepo.com/show/79251/crescent-moon.svg')";
     }
-    console.log(themeMode);
 }
